9eedbeff21da50ab7164b4c0b31452733f992f33
Shaposhnikov Maksym <shaposhnikoff@gmail.com>
2025-04-02 18:41:10 +0300

    ыыыы

--- create_graph_her_hour.py
+++ create_graph_her_hour.py
@@ -19,11 +19,9 @@ import string
 import csv
 
 defaault_path = '/Volumes/NetworkBackupShare/shelly'
-#iterate over the list of files shellyStats
-# list all the files in the directory
+
 def list_files(directory):
-    # Use glob to find all files in the directory
-    files = glob.glob(os.path.join(directory, '*'))
+    files = glob.glob(os.path.join(directory, '*.csv'))
     return files
 
 def create_voltage_graph_from_csv_file(csv_file):
@@ -66,21 +64,47 @@ def create_current_graph_from_csv_file(csv_file):
     
     plt.xlabel('Time')
     plt.ylabel('Current (A)')
-    #plt.title('Current Over Time (All Phases)')
-    # make title with timeperiod
     plt.title(f'Current Over Time (All Phases) {df["timestamp"].min()} - {df["timestamp"].max()}')
     plt.legend()
     plt.grid()
-    
-    # Save the plot to a file
     plt.savefig(f"{os.path.splitext(csv_file)[0]}_current_graph_all_phases.png")
     
 
-for file in list_files('/Volumes/NetworkBackupShare/shelly'):
-    print(file)
-    with open(file, 'r') as f:
-        file_contents = f.read()
-        #print(file_contents)
-        create_voltage_graph_from_csv_file(file)
-        create_current_graph_from_csv_file(file)
-        time.sleep(1)
+def merge_files_per_day():
+    data_by_date = {}
+    for file in list_files(defaault_path):
+
+        print(f"Processing file: {file}")
+        df = pd.read_csv(file)
+        df['date'] = pd.to_datetime(df['timestamp'], unit='s').dt.date
+        
+        for date, group in df.groupby('date'):
+            date_str = date.strftime('%Y-%m-%d')
+            
+            if date_str in data_by_date:
+                data_by_date[date_str] = pd.concat([data_by_date[date_str], group], ignore_index=True)
+            else:
+                data_by_date[date_str] = group
+
+    for date_str, data in data_by_date.items():
+        data = data.drop(columns=['date'])
+        output_folder = os.path.join(defaault_path, date_str)
+        os.makedirs(output_folder, exist_ok=True)
+        
+        output_file = os.path.join(output_folder, f"{date_str}.csv")
+        data.to_csv(output_file, index=False)
+        print(f"Saved merged file: {output_file}")
+
+# Запуск функции
+merge_files_per_day()
+create_voltage_graph_from_csv_file('/Volumes/NetworkBackupShare/shelly/2025-04-02/2025-04-02.csv')
+create_current_graph_from_csv_file('/Volumes/NetworkBackupShare/shelly/2025-04-02/2025-04-02.csv')
+
+# for file in list_files('/Volumes/NetworkBackupShare/shelly'):
+#     print(file)
+#     with open(file, 'r') as f:
+#         file_contents = f.read()
+#         #print(file_contents)
+#         create_voltage_graph_from_csv_file(file)
+#         create_current_graph_from_csv_file(file)
+#         time.sleep(1)
